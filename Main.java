2f02081abaacc48e38e46e364e6dea3fff0d5b40
student Bhavana Chandrakant <bbhavanachandrakant@gmail.com>
2026-02-14 18:47:41 +0530

    replace cocept

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
+class Main {
+    public static void main(String args[])
+    {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        String name = "bhavana";
+        String name1 = "bhavana";
         String name2 = "Chandrakant";
-        String name3=System.out.println(name.replace("a","b"));
-        System.out.println(name3);
+        String name3= name1+name2;
+        String name4=name3.replace("a","b");
+        System.out.println(name4);
     }
 }
